c541c0ffa60146db41a770b7d44dd6f7a12fd433
dariober <dario.beraldi@gmail.com>
2020-11-23 14:03:47 +0000

    Set token in covr.R script

--- tests/covr.R
+++ tests/covr.R
@@ -1,4 +1,4 @@
 library(covr)
 setwd('tests')
-xcov<- file_coverage(source_files= '../R/simulation_runner.R', test_files=  c('test_simulation_runner.R'))
-codecov(coverage= xcov)
+xcov <- file_coverage(source_files= '../R/simulation_runner.R', test_files=  c('test_simulation_runner.R'))
+codecov(coverage= xcov, token= '853ba810-ff8b-4cc2-9834-3f3817687feb') # Get token from https://app.codecov.io/gh/dariober/simulate-parasite-infection/settings
